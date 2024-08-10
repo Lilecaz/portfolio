44b03d6f167643b47697e8b91d6c5d2d58cb8d6b
Lilecaz <lilecaz0090@gmail.com>
2024-08-11 01:06:59 +0200

    fix build

--- src/pages/projets/adm4u.js
+++ src/pages/projets/adm4u.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import Header from '../../components/Header';
 import { ChevronLeftIcon } from '@heroicons/react/16/solid';
